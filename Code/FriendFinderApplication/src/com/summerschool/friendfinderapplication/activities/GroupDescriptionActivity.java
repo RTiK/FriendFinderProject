98d5348cc54a698f98c64f63403eeb3cb254e43b
stephanejaouannet <stephane.jaouannet@gmail.com>
2014-08-12 13:57:01 +0200

    last changes

--- Code/FriendFinderApplication/src/com/summerschool/friendfinderapplication/activities/GroupDescriptionActivity.java
+++ Code/FriendFinderApplication/src/com/summerschool/friendfinderapplication/activities/GroupDescriptionActivity.java
@@ -337,10 +337,14 @@ public class GroupDescriptionActivity extends Activity {
 	private void populateListView() {
 		ListView memberlist = (ListView) findViewById(R.id.memberListView);
 		ListView poilist = (ListView) findViewById(R.id.poiListView);
-		ListView eventlist = (ListView) findViewById(R.id.eventListView);
+		ListView eventlist = (ListView) findViewById(R.id.eventslistview);
 		memberlist.setAdapter(adapter);
 		poilist.setAdapter(POIadapter);
+<<<<<<< .mine
+		eventlist.setAdapter(Eventadapter);
+=======
 		eventlist.setAdapter(eventAdapter);
+>>>>>>> .r271
 	}	
 
 	public void onClickMapButton(final View v) {
